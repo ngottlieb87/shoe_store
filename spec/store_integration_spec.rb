a4d458debbe9faafcf0931955c57300c56011d96
Guest User <Guest@Epicodus-3A.local>
2017-10-02 19:11:03 -0700

    clean up formatting

--- spec/store_integration_spec.rb
+++ spec/store_integration_spec.rb
@@ -1,6 +1,7 @@
 require('spec_helper')
 
   describe 'The store creation path', {:type=>:feature} do
+
     it 'lets the user create stores and add them to a list' do
       visit '/add_store'
       fill_in('name', :with => "Tina's Shoe Shop")
